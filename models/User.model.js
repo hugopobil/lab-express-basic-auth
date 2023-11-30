314d19952c3db8707d1e8b34b3e31be7fcbe8f9c
Hugo Pobil <hpascual@mrhouston.net>
2023-11-30 20:51:39 +0100

    initial commit

--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,6 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const SALT_ROUNDS = 10;
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
@@ -8,6 +10,24 @@ const userSchema = new Schema({
   },
   password: String
 });
+
+userSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    bcrypt
+      .hash(this.password, SALT_ROUNDS)
+      .then((hash) => {
+        this.password = hash;
+        next();
+      })
+      .catch((error) => next(error));
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.checkPassword = function (passwordToCheck) {
+  return bcrypt.compare(passwordToCheck, this.password);
+};
 
 const User = model("User", userSchema);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,61 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/register", (req, res, next) => {
+  res.render("register");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+router.post("/register", (req, res, next) => {
+  // console.log(req.body);
+
+  User.create(req.body)
+    .then(() => {
+      res.redirect("/login");
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/login", (req, res, next) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        return user.checkPassword(password).then((match) => {
+          if (match) {
+            console.log("Te has logueado bien!!");
+            res.redirect(`/profile/${user._id}`);
+          } else {
+            console.log("Email o contraseña incorrectos"); // contraseña incorrecta
+            renderWithErrors();
+          }
+        });
+      } else {
+        console.log("Email o contraseña incorrectos"); // no existe usuario con ese email
+        renderWithErrors();
+      }
+    })
+    .catch((err) => next(err));
+  });
+
+router.get("/profile/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  User.findById(id)
+    .then((user) => {
+      res.render("profile", { user });
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;
